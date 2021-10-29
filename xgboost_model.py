712ec6c4c584c74d0571c98b7080efb6935991cf
Remy Stewart <75761497+remypstewart@users.noreply.github.com>
2021-10-29 08:02:05 -0400

    Update xgboost_model.py

--- xgboost_model.py
+++ xgboost_model.py
@@ -4,7 +4,6 @@ from yellowbrick.regressor import residuals_plot, prediction_error
 import pandas as pd
 import xgboost
 from numpy import absolute, sort, mean, ones
-import matplotlib.pyplot as pyplot
 import seaborn as sns
 
 ## initial view
@@ -46,3 +45,29 @@ scores = absolute(scores)
 print('MAE: %.3f (%.3f)' % (scores.mean(), scores.std()))
 
 residuals_plot(xgb_reg, feature_train, rent_train, feature_test, rent_test)
+
+## grid search
+params_test={'max_depth': [4,6,8],
+        'gamma': [0, 1, 2],
+        'eta': [0.01, 0.1, 0.2],
+        'n_estimators': [50, 100],
+    }
+xgb_tune = xgboost.XGBRegressor(random_state = 200)
+grid_results = GridSearchCV(estimator=xgb_tune,
+                           param_grid=params_test,
+                           scoring='neg_mean_absolute_error')
+grid_results.fit(feature_train, rent_train)
+
+print("Best parameters:", grid_results.best_params_)
+print("Lowest MAE: ", (-grid_results.best_score_))## final model
+
+## final model
+xgb_final = xgboost.XGBRegressor(max_depth=8, n_estimators=100, eta=0.2, gamma=2,
+                                 random_state=400)
+xgb_final.fit(feature_train, rent_train)
+preds = xgb_final.predict(feature_test)
+final_mae = mean_absolute_error(rent_test, preds)
+print("RMSE: %f" % (final_mae))
+
+xgboost.plot_importance(xgb_final)
+
